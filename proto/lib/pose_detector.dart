ad641ec6344ce0d1007a221dd112f99b04352557
Josh <josh.e.stein@gmail.com>
2021-12-11 16:43:10 +0100

    Make `PoseDetector` a stand-alone class
    The class is responsible for computing keypoints.

--- proto/lib/pose_detector.dart
+++ proto/lib/pose_detector.dart
@@ -19,57 +19,29 @@
 //   }
 // }
 
+import 'dart:collection';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tflite/tflite.dart';
 
-class PoseDetector extends StatefulWidget {
-  const PoseDetector({Key? key, required this.imagePath}) : super(key: key);
-
+class PoseDetector {
   final String imagePath;
 
-  @override
-  _PoseDetectorState createState() => _PoseDetectorState();
-}
-
-class _PoseDetectorState extends State<PoseDetector> {
-  File? _image;
-  bool _busy = false;
-  List _recognitions = [];
-  double _imageHeight = 0;
-  double _imageWidth = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _busy = true;
-
-    loadModel().then((value) {
-      setState(() {
-        _busy = false;
-      });
-    });
-  }
+  PoseDetector(this.imagePath);
 
   Future loadModel() async {
     await Tflite.loadModel(
         model: 'assets/posenet_mv1_075_float_from_checkpoints.tflite');
   }
 
-  Future predictImage() async {
-    var image = File(widget.imagePath);
+  Future getImageSize() async {
+    var image = File(imagePath);
     var decodedImage = await decodeImageFromList(image.readAsBytesSync());
-
-    setState(() {
-      _busy = true;
-      _image = image;
-      _imageHeight = decodedImage.height.toDouble();
-      _imageWidth = decodedImage.width.toDouble();
-    });
-    await poseNet(File(widget.imagePath));
+    var width = decodedImage.width.toDouble();
+    var height = decodedImage.height.toDouble();
+    return [width, height];
   }
 
   Future poseNet(File image) async {
@@ -79,83 +51,32 @@ class _PoseDetectorState extends State<PoseDetector> {
       numResults: 1,
     );
 
-    print(recognitions);
-
-    setState(() {
-      _recognitions = recognitions!;
-    });
+    // print(recognitions);
     int endTime = DateTime.now().millisecondsSinceEpoch;
     print("Inference took ${endTime - startTime}ms");
+    return recognitions;
   }
 
-  List<Widget> renderKeypoints(Size screen) {
+  Future<HashMap<String, List<double>>> getKeypoints(Size screen) async {
+    await loadModel();
+    var imageSize = await getImageSize();
+    var recognitions = await poseNet(File(imagePath));
+
+    double width = imageSize[0];
+    double height = imageSize[0];
+
     double factorX = screen.width;
-    double factorY = _imageHeight / _imageWidth * screen.width;
+    double factorY = height / width * screen.width;
 
-    var lists = <Widget>[];
-    for (var re in _recognitions) {
-      var color = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
-          .withOpacity(1.0);
-      var list = re["keypoints"].values.map<Widget>((k) {
-        return Positioned(
-          left: k["x"] * factorX - 6,
-          top: k["y"] * factorY - 6,
-          width: 100,
-          height: 12,
-          child: Text(
-            "● ${k["part"]}",
-            style: TextStyle(
-              color: color,
-              fontSize: 12.0,
-            ),
-          ),
-        );
-      }).toList();
-
-      lists.addAll(list);
+    HashMap<String, List<double>> hash = HashMap();
+    for (var re in recognitions) {
+      for (var keypoints in re["keypoints"].values) {
+        var x = keypoints["x"] * factorX;
+        var y = keypoints["y"] * factorY;
+        hash.putIfAbsent(keypoints["part"], () => [x, y]);
+      }
     }
-
-    return lists;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    List<Widget> stackChildren = [];
-
-    stackChildren.add(Positioned(
-      top: 0.0,
-      left: 0.0,
-      width: size.width,
-      // TODO: switch to future builder
-      child: _image == null
-          ? const Text('No image selected.')
-          : Image.file(_image!),
-    ));
-
-    stackChildren.addAll(renderKeypoints(size));
-
-    if (_busy) {
-      stackChildren.add(const Opacity(
-        child: ModalBarrier(dismissible: false, color: Colors.grey),
-        opacity: 0.3,
-      ));
-      stackChildren.add(const Center(child: CircularProgressIndicator()));
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PoseNet'),
-      ),
-      body: Stack(
-        children: stackChildren,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: predictImage,
-        tooltip: 'Pick Image',
-        child: const Icon(Icons.image),
-      ),
-    );
+    print(hash);
+    return hash;
   }
 }
