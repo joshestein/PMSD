149ad428d6e5ff2057dd82ff27a3d43f544cb337
Josh <josh.e.stein@gmail.com>
2022-01-01 16:17:24 +0100

    Add new height/weight data form screen

---
+++ proto/lib/height_weight_confirmation.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HeightWeightConfirmation extends StatefulWidget {
+  const HeightWeightConfirmation({Key? key, required this.height})
+      : super(key: key);
+
+  final double height;
+
+  @override
+  _HeightWeightConfirmationState createState() =>
+      _HeightWeightConfirmationState();
+}
+
+class _HeightWeightConfirmationState extends State<HeightWeightConfirmation> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Height and Weight Data"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  initialValue: widget.height.toStringAsFixed(2),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Height',
+                    suffixText: 'cm',
+                  ),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Weight',
+                    suffixText: 'kg',
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Submit'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
