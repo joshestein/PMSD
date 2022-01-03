cb9db5a187845da34bfd2668e90fc2fdffeeb15f
Josh <josh.e.stein@gmail.com>
2022-01-03 18:30:25 +0100

    Initialize db in main

--- proto/lib/main.dart
+++ proto/lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:proto/db_helper.dart';
 import 'package:proto/image_picker.dart';
+import 'package:sqflite/sqflite.dart';
+
+late Database db;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  db = await DatabaseHelper().getDb();
+
   runApp(MaterialApp(
     theme: ThemeData.dark(),
     home: const ImagePickerScreen(),
-    // home: const ImagePreview(),
   ));
 }
