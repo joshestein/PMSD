694da2f8f246cef404d216e65473260cf21686ae
Josh <josh.e.stein@gmail.com>
2022-01-03 17:06:33 +0100

    Standard Hough transform line detection

--- opencv/lines.cpp
+++ opencv/lines.cpp
@@ -22,6 +22,22 @@ int main(int argc, char **argv) {
   cvtColor(dst, cdst, COLOR_GRAY2BGR);
   cdstP = cdst.clone();
 
+  // Standard Hough Line Transform
+  std::vector<Vec2f> lines;
+  HoughLines(dst, lines, 1, CV_PI / 180, 80, 0, 0);
+  // Draw the lines
+  for (size_t i = 0; i < lines.size(); i++) {
+    float rho = lines[i][0], theta = lines[i][1];
+    Point pt1, pt2;
+    double a = cos(theta), b = sin(theta);
+    double x0 = a * rho, y0 = b * rho;
+    pt1.x = cvRound(x0 + 1000 * (-b));
+    pt1.y = cvRound(y0 + 1000 * (a));
+    pt2.x = cvRound(x0 - 1000 * (-b));
+    pt2.y = cvRound(y0 - 1000 * (a));
+    line(cdst, pt1, pt2, Scalar(0, 0, 255), 3, LINE_AA);
+  }
+
   // Probabilistic Hough Transform
   std::vector<Vec4i> linesP;
   HoughLinesP(dst, linesP, 1, CV_PI / 180, 80, 30, 10);
@@ -32,6 +48,7 @@ int main(int argc, char **argv) {
     line(cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, LINE_AA);
   }
   imshow("Source", image);
+  imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst);
   imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP);
   while (waitKey() != 27)
     ;
