1515959918352a490fd4b210f03158af082b7ed9
Josh <josh.e.stein@gmail.com>
2022-01-03 21:11:34 +0100

    Make Child DAO ID optional

--- proto/lib/models/child.dart
+++ proto/lib/models/child.dart
@@ -3,21 +3,21 @@ import 'package:sqflite/sqflite.dart';
 import '../main.dart';
 
 class Child {
-  final int id;
   final int parentId;
+  final int? id;
   final String? name;
   final int? ageInMonths;
 
   Child({
-    required this.id,
     required this.parentId,
+    this.id,
     this.name,
     this.ageInMonths,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
+      'child_id': id,
       'parent_id': parentId,
       'name': name,
       'age_in_months': ageInMonths,
