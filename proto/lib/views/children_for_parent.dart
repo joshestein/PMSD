b5f0756c9205b9a4c87388ad198c10699249b0e0
Josh <josh.e.stein@gmail.com>
2022-01-04 09:08:12 +0100

    Expandable children for given parent widget
    For each parent, we can asynchronously fetch child information for
    efficient database polling.

---
+++ proto/lib/views/children_for_parent.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:proto/models/child.dart';
+import 'package:proto/models/parent.dart';
+
+class ChildrenForParent extends StatefulWidget {
+  final Parent parent;
+
+  const ChildrenForParent({Key? key, required this.parent}) : super(key: key);
+
+  @override
+  _ChildrenForParentState createState() => _ChildrenForParentState();
+}
+
+class _ChildrenForParentState extends State<ChildrenForParent> {
+  List<Child> _children = [];
+  bool _fetched = false;
+
+  // Prefer asynchronously pulling children for a particular parent to avoid
+  // fetching all parents and children at initilization
+  void _fetchChildren() async {
+    int parentId =
+        widget.parent.id ?? await getParentId(widget.parent.idCardNo);
+    List<Child> children = await getChildrenForParent(parentId);
+
+    setState(() {
+      _children = children;
+      _fetched = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+        title: Text(widget.parent.idCardNo),
+        onExpansionChanged: (value) {
+          if (!_fetched) {
+            _fetchChildren();
+          }
+        },
+        children: [
+          ListView.builder(
+            itemCount: _children.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_children[index].name),
+                subtitle: Text(_children[index].ageInMonths ?? ''),
+              );
+            },
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+          ),
+        ]);
+  }
+}
